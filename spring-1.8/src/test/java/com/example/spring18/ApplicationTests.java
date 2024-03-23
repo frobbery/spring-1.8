5355f5a6674f65a415c3cf292f8dd7abfd11ff7f
Yuliya Puzina <femalerobbery68@gmail.com>
2024-03-23 21:45:15 +0300

    fix tests

--- spring-1.8/src/test/java/com/example/spring18/ApplicationTests.java
+++ spring-1.8/src/test/java/com/example/spring18/ApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.spring18;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.shell.interactive.enabled=false"})
+@SpringBootTest(properties = {"spring.shell.interactive.enabled=false",
+		"spring.datasource.url=jdbc:h2:mem:testdb", "spring.datasource.driver-class-name=org.h2.Driver"})
 class ApplicationTests {
 
 	@Test
